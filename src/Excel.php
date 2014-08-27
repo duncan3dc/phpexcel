6058314c5b44e50b6be98d3f5a29437ced523741
Craig Duncan <git@duncanc.co.uk>
2014-08-27 11:11:33 +0100

    Tidied up to conform to PSR coding standards/style

--- src/Excel.php
+++ src/Excel.php
@@ -2,8 +2,8 @@
 
 namespace duncan3dc\Phpexcel;
 
-class Excel extends \PHPExcel {
-
+class Excel extends \PHPExcel
+{
     const BOLD    =   1;
     const ITALIC  =   2;
     const LEFT    =   4;
@@ -11,48 +11,44 @@ class Excel extends \PHPExcel {
     const CENTER  =  16;
 
 
-    public static function read($filename,$key=-1) {
-
+    public static function read($filename, $key = -1)
+    {
         $type = \PHPExcel_IOFactory::identify($filename);
         $reader = \PHPExcel_IOFactory::createReader($type);
         $reader->setReadDataOnly(true);
         $excel = $reader->load($filename);
 
-        if(is_string($key)) {
+        if (is_string($key)) {
             $sheet = $excel->getSheetByName($key);
-        } elseif($key > -1) {
+        } elseif ($key > -1) {
             $sheet = $excel->getSheet($key);
         } else {
-            $sheets = array();
-            foreach($excel->getWorksheetIterator() as $sheet) {
-                $sheets[] = $sheet->toArray(null,false,false,false);
+            $sheets = [];
+            foreach ($excel->getWorksheetIterator() as $sheet) {
+                $sheets[] = $sheet->toArray(null, false, false, false);
             }
             return $sheets;
         }
 
-        return $sheet->toArray(null,false,false,false);
-
+        return $sheet->toArray(null, false, false, false);
     }
 
 
-    public function save($filename) {
-
-        $writer = new \PHPExcel_Writer_Excel2007($this);
-
-        return $writer->save($filename);
-
+    public function save($filename)
+    {
+        return (new \PHPExcel_Writer_Excel2007($this))->save($filename);
     }
 
 
-    public function output($filename) {
-
-        $tmp = tempnam("/tmp","excel_");
+    public function output($filename)
+    {
+        $tmp = tempnam("/tmp", "excel_");
 
         $this->save($tmp);
 
         $data = file_get_contents($tmp);
 
-        if(substr($filename,-5) != ".xlsx") {
+        if (substr($filename, -5) != ".xlsx") {
             $filename .= ".xlsx";
         }
 
@@ -63,23 +59,22 @@ class Excel extends \PHPExcel {
         echo $data;
 
         unlink($tmp);
-
     }
 
 
-    public static function getCellName($col,$row) {
-
+    public static function getCellName($col, $row)
+    {
         $cell = "";
 
         $col += 65;
 
         $prefix = 64;
-        while($col > 90) {
+        while ($col > 90) {
             $prefix++;
             $col -= 26;
         }
 
-        if($prefix > 64) {
+        if ($prefix > 64) {
             $cell .= chr($prefix);
         }
 
@@ -88,65 +83,60 @@ class Excel extends \PHPExcel {
         $cell .= $row;
 
         return $cell;
-
     }
 
 
-    public function setCell($cell,$value,$style=false) {
-
-        preg_match("/^([a-z]+)([0-9]+)$/i",$cell,$matches);
+    public function setCell($cell, $value, $style = null)
+    {
+        preg_match("/^([a-z]+)([0-9]+)$/i", $cell, $matches);
         $col = $matches[1];
         $row = $matches[2];
 
-        $this->getActiveSheet()->SetCellValue($cell,$value);
+        $this->getActiveSheet()->SetCellValue($cell, $value);
 
         $this->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
 
-        if(!$style) {
+        if (!$style) {
             return true;
         }
 
-        if($style & static::BOLD) {
+        if ($style & static::BOLD) {
             $this->getActiveSheet()->getStyle($cell)->getFont()->setBold(true);
         }
 
-        if($style & static::ITALIC) {
+        if ($style & static::ITALIC) {
             $this->getActiveSheet()->getStyle($cell)->getFont()->setItalic(true);
         }
 
-        if($style & static::LEFT) {
+        if ($style & static::LEFT) {
             $this->getActiveSheet()->getStyle($cell)->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
         }
 
-        if($style & static::RIGHT) {
+        if ($style & static::RIGHT) {
             $this->getActiveSheet()->getStyle($cell)->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
         }
 
-        if($style & static::CENTER) {
+        if ($style & static::CENTER) {
             $this->getActiveSheet()->getStyle($cell)->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
         }
-
     }
 
 
-    public function addImage($cell,$path) {
-
+    public function addImage($cell, $path)
+    {
         $image = new \PHPExcel_Worksheet_Drawing();
 
-        $name = pathinfo($path,PATHINFO_BASENAME);
+        $name = pathinfo($path, PATHINFO_BASENAME);
         $image->setName($name);
 
         $image->setPath($path);
 
-        $row = preg_replace("/^[a-z]+([0-9]+)$/i","$1",$cell);
+        $row = preg_replace("/^[a-z]+([0-9]+)$/i", "$1", $cell);
         $image->setHeight(50);
         $this->getActiveSheet()->getRowDimension($row)->setRowHeight(50);
 
         $image->setCoordinates($cell);
 
         $image->setWorksheet($this->getActiveSheet());
-
     }
-
-
 }
